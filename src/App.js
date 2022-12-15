c5619f5e9012b35a24df70149e02b11fbbd287a0
pal404blog <palnft69@gmail.com>
2022-12-15 13:35:20 +0530

    basic

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import { Head } from './page/Head'
+import { Footer } from './page/Footer';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Router,
+} from "react-router-dom";
+import { NavItem } from 'react-bootstrap';
+
+
+function App() {
+
+  const data = (id) =>{
+
+  }
+  return (
+    <div className="App">
+      <BrowserRouter>
+
+{/* <Header title="Todo List" searchBar={true} /> */}
+<Head />
+<Routes>
+  {/* <Route exact path="/" element={<Shows />} />
+  <Route exact path="about" element={<ShowAbout/>} /> */}
+</Routes>
+<Footer />
+
+</BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
