57cbc8ef811fbb229be3bd8fd2fd62be7c669939
Jayur <pjayur14@gmail.com>
2023-01-02 12:17:56 +0530

    ?

--- src/page/Test.js
+++ src/page/Test.js
@@ -3,9 +3,11 @@ import React from 'react'
 
 export const Test = () => {
     return (
+      <>
       <div >Test 30000</div>
       <div> TEst Pal</div>
       <div> Test new</div>
+      </>
     )
   }
   
